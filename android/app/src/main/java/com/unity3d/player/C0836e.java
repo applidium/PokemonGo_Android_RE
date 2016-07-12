53fa2d41a7d14c8c8f70e8c529714d2e07f9b2d4
Adrien Couque <adrien.couque@applidium.com>
2016-07-12 18:10:41 +0200

    Replace unity code with jar provided on project export

--- android/app/src/main/java/com/unity3d/player/C0836e.java
+++
@@ -1,9 +0,0 @@
-package com.unity3d.player;
-
-import android.view.MotionEvent;
-import android.view.View;
-
-/* renamed from: com.unity3d.player.e */
-public interface C0836e {
-    boolean m115a(View view, MotionEvent motionEvent);
-}
--- android/app/src/main/java/com/unity3d/player/NativeLoader.java
+++
@@ -1,7 +0,0 @@
-package com.unity3d.player;
-
-public class NativeLoader {
-    static final native boolean load(String str);
-
-    static final native boolean unload();
-}
