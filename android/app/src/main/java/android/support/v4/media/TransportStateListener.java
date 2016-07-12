c1734e8d3842c1ed89c5b4cc067987b1f9c99c30
Adrien Couque <adrien.couque@applidium.com>
2016-07-12 11:41:34 +0200

    Replace android.support code by gradle dependencies

--- android/app/src/main/java/android/support/v4/media/TransportStateListener.java
+++
@@ -1,9 +0,0 @@
-package android.support.v4.media;
-
-public class TransportStateListener {
-    public void onPlayingChanged(TransportController transportController) {
-    }
-
-    public void onTransportControlsChanged(TransportController transportController) {
-    }
-}
